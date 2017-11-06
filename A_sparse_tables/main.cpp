64a8b487d3476c264eb1008784e5142e5d6a8741
fogside <cheskidova@phystech.edu>
2017-11-07 01:16:18 +0300

    fix: optimised input

--- A_sparse_tables/main.cpp
+++ A_sparse_tables/main.cpp
@@ -9,22 +9,22 @@ private:
     vector<vector<long> > table;
     vector<int> K;
 public:
-    explicit SparseTable(vector<long> &data);
+    explicit SparseTable(int a1, long n);
 
     long RMQ(unsigned long u, unsigned long v);
 };
 
-SparseTable::SparseTable(vector<long> &data) : table(int(log2(data.size())) + 1, vector<long>(data.size())),
-                                               K(data.size()) {
+SparseTable::SparseTable(int a1, long n) : table((ilogb(n+1)) + 1, vector<long>(n+1)), K(n+1) {
 
-    for (int j = 1; j < data.size(); ++j){
-        K[j] = int(log2(j));
-        table[0][j] = data[j];
+    table[0][1] = a1;
+    for (long i = 2; i <= n; ++i){
+        table[0][i] = (23 * table[0][i - 1] + 21563) % 16714589;
+        K[i] = ilogb(i);
     }
 
     for (int k = 1; k < table.size(); ++k) {
         unsigned long p = pow(2, k - 1);
-        for (unsigned long i = 1; i + p <= data.size(); ++i)
+        for (unsigned long i = 1; i + p <= n+1; ++i)
             table[k][i] = min(table[k - 1][i], table[k - 1][i + p]);
     }
 }
@@ -41,17 +41,12 @@ long SparseTable::RMQ(unsigned long u, unsigned long v) {
 int main() {
     unsigned long n, m, v1, u1;
     int a1;
+
     cin >> n >> m >> a1;
     cin >> u1 >> v1;
 
-    // data formation
-    vector<long> data(n + 1);
-    data[1] = a1;
-    for (int i = 2; i <= n; ++i)
-        data[i] = (23 * data[i - 1] + 21563) % 16714589;
-
     // ST formation
-    SparseTable sparseTable(data);
+    SparseTable sparseTable(a1, n);
 
     // requests formation
     //ui + 1 = (17 · ui + 751 + ansi + 2i) % n + 1,
