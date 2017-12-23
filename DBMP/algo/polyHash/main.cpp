12cf3b881dc2eb7ec465450e0232855ff8eea7a8
fogside <cheskidova@phystech.edu>
2017-12-24 00:06:55 +0300

    feat: added reads file to search for multiple reads

--- DBMP/algo/polyHash/main.cpp
+++ DBMP/algo/polyHash/main.cpp
@@ -2,33 +2,92 @@
 #include <fstream>
 #include <vector>
 
-#define K 11
+/*
+ * На вход программе подаётся референсная строка и fasta-файл с ридами произвольной длины.
+ * Необходимо найти все вхождения ридов в референсную последовательность, используя полиномиальные хеши.
+ */
 
+
+#define K 11
 using namespace std;
 
-void compute_powers(vector<unsigned long int> &powers);
+void compute_powers(vector<unsigned long int> &powers) {
+    powers[0] = 1;
+    for (int i = 1; i < powers.size(); i++)
+        powers[i] = powers[i - 1] * K;
+}
+
+void find_occurences(string &read, u_long ref_size,
+                     vector<unsigned long int> &ref_hashes,
+                     vector<unsigned long int> &powers) {
+
+    unsigned long int prefix_hash;
+    unsigned long int read_hash;
+    bool found = false;
+
+    // Compute hash for read
+    read_hash = 0;
+    for (int i = 0; i < read.size(); i++)
+        read_hash += powers[i] * (long int) read[i];
+
+    cout << "FOR READ: " << read << '\n';
+    // Compare hashes. NOTE: for many reads you should precompute prefix hashes:
+    // ref_hashes[i + read.size() - 1] - ref_hashes[i-1]
+    if (read_hash == ref_hashes[read.size() - 1]) {
+        found = true;
+        printf("Read found at position: 0");
+    }
+    for (int i = 1; i < ref_size - read.size() + 1; i++) {
+        prefix_hash = ref_hashes[i + read.size() - 1] - ref_hashes[i - 1];
+        if (prefix_hash == read_hash * powers[i]) {
+            found = true;
+            printf("Read found at position: %i\n", i);
+        }
+    }
+    if(!found)
+        cout<<"Nothing found!"<<'\n';
+    cout << '\n';
+}
 
 int main() {
-    const string INPUT_FILE = "/home/fogside/Projects/algorithms/DBMP/algo/polyHash/reference.txt";
+    const string reference_path = "/home/fogside/Projects/algorithms/DBMP/algo/polyHash/reference.txt";
+    const string read_path = "/home/fogside/Projects/algorithms/DBMP/algo/polyHash/reads.txt";
+
     string reference;
-    string read = "TLEN";
+    vector<string> reads;
 
     unsigned long int prefix_hash;
     unsigned long int read_hash;
 
 
-    ifstream input_file(INPUT_FILE);
-    if (input_file.is_open()) {
-        getline(input_file, reference);
+    ifstream ref_file(reference_path);
+    ifstream reads_file(read_path);
+
+    if (ref_file.is_open()) {
+        getline(ref_file, reference);
     } else {
-        cout << "Can't open input file!" << '\n';
+        cout << "Can't open ref file!" << '\n';
         return 1;
     }
+
+    if (reads_file.is_open()) {
+        string r;
+        while (getline(reads_file, r)) {
+            reads.push_back(r);
+        }
+    } else {
+        cout << "Can't open reads file!" << '\n';
+        return 1;
+    }
+
+    ref_file.close();
+    reads_file.close();
+
 
     vector<unsigned long int> powers(reference.size());
     vector<unsigned long int> ref_hashes(reference.size());
 
-    // Precompute all powers of the
+    // Precompute all powers
     compute_powers(powers);
 
     // Compute hashes for reference prefixes
@@ -36,25 +95,7 @@ int main() {
     for (int i = 1; i < reference.size(); i++)
         ref_hashes[i] = (long int) reference[i] * powers[i] + ref_hashes[i - 1];
 
-
-    // Compute hash for read
-    read_hash = 0;
-    for (int i = 0; i < read.size(); i++)
-        read_hash += powers[i] * (long int) read[i];
-
-    // Compare hashes. NOTE: for many reads you should precompute prefix hashes:
-    // ref_hashes[i + read.size() - 1] - ref_hashes[i-1]
-    if (prefix_hash == ref_hashes[read.size() - 1])
-        printf("Read found at position: 0");
-    for (int i = 1; i < reference.size() - read.size() + 1; i++) {
-        prefix_hash = ref_hashes[i + read.size() - 1] - ref_hashes[i - 1];
-        if (prefix_hash == read_hash * powers[i])
-            printf("Read found at position: %i\n", i);
+    for (auto read: reads) {
+        find_occurences(read, reference.size(), ref_hashes, powers);
     }
-}
-
-void compute_powers(vector<unsigned long int> &powers) {
-    powers[0] = 1;
-    for (int i = 1; i < powers.size(); i++)
-        powers[i] = powers[i - 1] * K;
 }
